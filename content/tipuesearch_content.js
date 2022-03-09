160133ed4824d7e189416805e4a49cc223594f76
a40923101 <a40923101@github.com>
2022-03-09 11:22:59 +0800

    title

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n tes123123 \n 11111111 \n 40923101', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n tes123123 \n 11111111 \n', 'tags': '', 'url': 'About.html'}, {'title': '40923101', 'text': '', 'tags': '', 'url': '40923101.html'}]};
