69d215ef8a0781459305c1bb36e3dbca6aec8634
40923108 <40923108@gm.nfu.edu.tw>
2022-03-13 21:02:11 +0800

    組員名單

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-var tipuesearch = {"pages": [{'title': 'About', 'text': '', 'tags': '', 'url': 'About.html'}, {'title': '40923101', 'text': '', 'tags': '', 'url': '40923101.html'}, {'title': '40923108', 'text': '', 'tags': '', 'url': '40923108.html'}]};
-=======
-<<<<<<< HEAD
-var tipuesearch = {"pages": [{'title': 'About', 'text': '', 'tags': '', 'url': 'About.html'}, {'title': '40923103', 'text': '', 'tags': '', 'url': '40923103.html'}]};
-=======
-
->>>>>>> e5fe8b960fda99a68d6d0168fd4b5c612118972d
->>>>>>> e937cc4520f6ef226529508b9853d0db08b058f0
+var tipuesearch = {"pages": [{'title': 'About', 'text': '第一大組 \n 組長： \n 4092318 \n 組員： \n 40923101 \n 40923103 \n 40923119 \n 40923137 \n 40923154 \n 40923156 \n 40971134', 'tags': '', 'url': 'About.html'}, {'title': '40923101', 'text': '3/13 \n', 'tags': '', 'url': '40923101.html'}, {'title': '40923103', 'text': '3/14 \n', 'tags': '', 'url': '40923103.html'}, {'title': '40923108', 'text': '', 'tags': '', 'url': '40923108.html'}]};
